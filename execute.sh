2f25aee3ca029703db05cbbd525ae13c40004c15
Squallium <709429+Squallium@users.noreply.github.com>
2026-04-02 20:28:02 +0200

    feat(templates): added file parameters to addons

---
+++ execute.sh
@@ -0,0 +1,2 @@
+cookiecutter ./templates/gitops/ -f --no-input --config-file ./templates/configs/gitops/config.yaml
+cookiecutter ./templates/kargo/ -f --no-input --config-file ./templates/configs/gitops/config.yaml
